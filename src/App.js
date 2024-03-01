781c87a57f1e7b358897602a8eb7a29cb5296e29
Snuggleapp <143851563+Snuggleapp@users.noreply.github.com>
2024-03-01 14:38:27 -0300

    teste2

--- src/App.js
+++ src/App.js
@@ -61,14 +61,6 @@ function App() {
           />
           <button onClick={adicionarItem}>Adicionar</button>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
 app.use(express.json());
 
 // Conexão com o MongoDB Atlas
@@ -55,6 +52,6 @@ app.post("/adicionarItem", async (req, res) => {
   }
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
